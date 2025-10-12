101167ab77b994a9d209f2b2c2e34766ae176a18
emrocode <92260849+emrocode@users.noreply.github.com>
2025-10-12 19:13:43 -0300

    Remove version

--- src/utils/help.mjs
+++ src/utils/help.mjs
@@ -1,5 +1,3 @@
-const { version } = require("./package.json");
-
 export const showHelp = () => {
-  echo`${chalk.bold(`Chrono ${version}`)}\n  A Wayback Machine CLI Link Generator\n\n${chalk.bold("Usage")}\n  npx -- @emrocode/chrono [options]\n\n${chalk.bold("Options")}\n  --help, -h    print help\n  --open, -o    open in a browser\n  --web=<URL>   set the base URL for monitoring`;
+  echo`${chalk.bold("Chrono")}\n  A Wayback Machine CLI Link Generator\n\n${chalk.bold("Usage")}\n  npx -- @emrocode/chrono [options]\n\n${chalk.bold("Options")}\n  --help, -h    print help\n  --open, -o    open in a browser\n  --web=<URL>   set the base URL for monitoring`;
 };
