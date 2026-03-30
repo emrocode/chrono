ef72c0ba6461ec90043ffe214e82c69ca767ec1f
emrocode <92260849+emrocode@users.noreply.github.com>
2026-03-30 14:50:25 -0300

    Correct year validation logic

--- src/utils/year.mjs
+++ src/utils/year.mjs
@@ -1,5 +1,5 @@
 export const getAndValidateYear = async ({ data, year }) => {
-  let years = [];
+  let yearSet = new Set();
 
   // save all timestamps into years[]
   for (let i = 1; i < data.length; i++) {
@@ -8,19 +8,18 @@ export const getAndValidateYear = async ({ data, year }) => {
     const match = timestamp.match(/^\d{4}/g);
 
     if (match) {
-      years.push(match[0]);
+      yearSet.add(match[0]);
     }
   }
 
-  echo(chalk.cyan("Available years:"), years);
+  const years = Array.from(yearSet).sort();
 
-  // if year flag is provided, validate it
   if (year) {
     const yearStr = String(year);
 
     if (!years.includes(yearStr)) {
       echo(chalk.yellow(`Year ${year} is not available in the archive.`));
-      echo(chalk.yellow(`Available years: ${years.join(",")}`));
+      echo(chalk.cyan(`Available years: ${years.join(",")}`));
       process.exit(1);
     }
 
@@ -28,16 +27,22 @@ export const getAndValidateYear = async ({ data, year }) => {
     return yearStr;
   }
 
-  const date = new Date();
-  let yearInput = (await question(chalk.cyan("Year: "))) || date.getFullYear();
+  echo(chalk.cyan("Available years:"), years);
 
+  const date = new Date();
+
+  let yearInput = (await question(chalk.cyan("Year: "))) || date.getFullYear();
   if (yearInput < years[0]) {
     echo(chalk.yellow(`Year ${yearInput} not found. Using ${years[0]}.`));
     yearInput = years[0];
   }
 
   if (yearInput > years.at(-1)) {
-    echo(chalk.yellow(`Year ${yearInput} not found. Using ${years.at(-1)}.`));
+    echo(
+      chalk.yellow(
+        `Year ${yearInput} exceeds available data. Using ${years.at(-1)}.`,
+      ),
+    );
     yearInput = years.at(-1);
   }
 
